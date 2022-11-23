bde80e7f72c16244b795650e988898610ac304b2
starkshn <starkshn97@gmail.com>
2022-11-23 18:00:31 +0900

    Index Buffer

--- Engine/Mesh.h
+++ Engine/Mesh.h
@@ -4,17 +4,26 @@
 class Mesh
 {
 public:
-	void Init(vector<Vertex>& vec);
+	void Init(const vector<Vertex>& vertexBuffer, const vector<uint32>& indexBuffer);
 	void Render(); 
 
 public:
 	void SetTransform(const Transform& t) { _transform = t; }
 
 private:
+	void CreateVertexBuffer(const vector<Vertex>& buffer);
+	void CreateIndexBuffer(const vector<uint32>& buffer);
+
+private:
 	ComPtr<ID3D12Resource>		_vertexBuffer;
 	D3D12_VERTEX_BUFFER_VIEW	_vertexBufferView = {}; // ID3D12Resource 를 가르키고 지칭하는 역할.
 	uint32						_vertexCount = 0;
 
-	Transform _transform = {};
+	// Index Buffer
+	ComPtr<ID3D12Resource>		_indexBuffer;
+	D3D12_INDEX_BUFFER_VIEW		_indexBufferView;
+	uint32						_indexCount = 0;
+
+	Transform					_transform = {};
 };
 
